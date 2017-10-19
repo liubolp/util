1e8e74f15a58d4e83a26c2b09f8fc4c387798fc0
liubo <liubolp@icloud.com>
2017-10-19 17:16:17 +0800

    init

---
+++ dist/calc_font_size.js
@@ -0,0 +1,16 @@
+/**
+ * @description 计算移动端跟元素的字体大小
+ * 默认移动端的设计是iPhone6尺寸
+ * 经过计算后1rem等于100px
+ */
+(function (document, window, size) {
+	var html = document.documentElement,
+			resizeEvent = 'orientationchange' in window ? 'orientationchange' : 'resize',
+			calcFontSize = function () {
+				var deviceWidth = html.clientWidth > 1300 ? 1300 : html.clientWidth;
+				html.style.fontSize = 100 * (deviceWidth / size) + 'px';
+			};
+	if (!document.addEventListener) {return;}
+	window.addEventListener(resizeEvent, calcFontSize, false);
+	document.addEventListener('DOMContentLoaded', calcFontSize, false);
+})(document, window, 750);
