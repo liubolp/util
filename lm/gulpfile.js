5e64346a0f8ee25ebdef2dcc0c835d3a36f449de
liubo <liubolp@icloud.com>
2017-10-26 18:21:58 +0800

    编写商品管理

--- lm/gulpfile.js
+++ lm/gulpfile.js
@@ -2,11 +2,31 @@ var gulp = require('gulp')
 var less = require('gulp-less')
 var soucemaps = require('gulp-sourcemaps')
 var postcss = require('gulp-postcss')
+var broswerSync = require('browser-sync')
 gulp.task('css', function () {
-  return gulp.src(['src/**/*.less'])
-    .pipe(soucemaps.init())
-    .pipe(less())
-    .pipe(postcss([require('precss'), require('autoprefixer')]))
-    .pipe(soucemaps.write('.'))
-    .pipe(gulp.dest('dist'))
+  return gulp.src(['src/**/*.less', '!src/common/*.less'])
+    //.pipe(soucemaps.init())
+    .pipe(less().on('error', function(e) {
+      console.error(e.message);
+      this.emit('end');
+    }))
+    .pipe(postcss([require('precss'), require('autoprefixer')(['iOS >= 7', 'Android >= 4.1'])]))
+    //.pipe(soucemaps.write('.'))
+    .pipe(gulp.dest('src'))
+    .pipe(broswerSync.reload({stream: true}))
+})
+gulp.task('server', function () {
+  broswerSync.init({
+    files: ['src/**/*.html', 'src/**/*.js'],
+    server: {
+      baseDir: 'src'
+    }
+  })
+})
+gulp.task('watch', function () {
+  gulp.watch(['src/**/*.less'], ['css'])
+})
+gulp.task('default', ['css'], function () {
+  gulp.start('watch')
+  gulp.start('server')
 })
