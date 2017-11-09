62b31e1017d94b79afc4a65bda23e2e21c18315f
liubo <liubolp@icloud.com>
2017-11-09 19:09:53 +0800

    修改样式

--- lm/src/customer/js/index.js
+++ lm/src/customer/js/index.js
@@ -1,1 +1,15 @@
-console.log(1)
+window.addEventListener('load', function () {
+  $(function () {
+    var swiper = new Swiper('.swiper-container', {
+      autoplay: 5000,
+      pagination: '.swiper-pagination',
+      loop: true
+    })
+    $('h3.title').click(function () {
+      $('.accordion .list').not($(this).next()).slideUp()
+      $(this).next().slideToggle()
+      $('.title .toggle').not($(this).find('.toggle')).removeClass('fold')
+      $(this).find('.toggle').toggleClass('fold')
+    })
+  })
+})
