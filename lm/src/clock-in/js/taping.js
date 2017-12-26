a4197103c66f993db54eed724c4b453cc6a29767
liubo <liubolp@icloud.com>
2017-12-26 18:24:21 +0800

    添加每日一听

---
+++ lm/src/clock-in/js/taping.js
@@ -0,0 +1,88 @@
+window.addEventListener('load', function () {
+  $(function () {
+    var obj = {
+      data: {
+        timer: null, // 倒计时操作
+        timeout: null, // 录音超时
+        limit: 10, // 最大录音时长
+        duration: 55 // 实际录制时长
+      },
+      init: function () {
+        this.event()
+      },
+      event: function () {
+        // 使用官方播报
+        $('.taping').on('click', '.official', function (e) {
+          $('.modal-official').show()
+        }) // 录制自定义语音
+          .on('click', '.record', function (e) {
+            if ($(this).hasClass('recording')) { // 结束录制
+              obj.methods.endRecord()
+              obj.methods.clearTimer()
+              $(this).removeClass('recording').text('')
+              $('.modal-record').show()
+                .find('.bar').text(obj.data.duration + '"')
+            } else { // 开始录制
+              obj.methods.countDown($(this).addClass('recording').text(obj.data.limit))
+              obj.methods.startRecord()
+            }
+          })
+        // 使用官方语音播报
+        $('.modal-official .generate').click(function (e) {
+          obj.methods.generate('official')
+          $('.modal-official').hide()
+        })
+        // 自定义语音播报
+        $('.modal-record').on('click', '.play', function (e) { // 播放当前录制语音
+          obj.methods.playVoice()
+        }).on('click', '.re', function (e) { // 重新录制
+          $('.modal-record').hide()
+          $('.taping .record').trigger('click')
+        }).on('click', '.generate', function (e) {
+          $('.modal-record').hide()
+          obj.methods.generate()
+        })
+      },
+      methods: {
+        // 生成语音播报
+        generate (type) {
+          if (type === 'official') {
+            // todo 使用官方播报
+          } else {
+            // todo 使用自定义语音播报
+          }
+        },
+        // 倒计时操作
+        countDown (target) {
+          obj.data.timeout = setTimeout(function () {
+            clearInterval(obj.data.timer)
+            $('.taping .record').trigger('click')
+          }, obj.data.limit * 1000)
+          obj.data.timer = setInterval(function () {
+            var count = target.text() - 1
+            target.text(count)
+          }, 1000)
+        },
+        // 清楚计时器
+        clearTimer () {
+          clearTimeout(obj.data.timeout)
+          clearInterval(obj.data.timer)
+        },
+        // 开始录制
+        startRecord () {
+          // todo 调用接口开始录制
+        },
+        // 结束录制
+        endRecord () {
+          // todo 结束录制并获取录制时长duration
+          obj.data.duration = 30 // (单位秒)
+        },
+        // 播放声音
+        playVoice () {
+          // todo
+        }
+      }
+    }
+    obj.init()
+  })
+})
