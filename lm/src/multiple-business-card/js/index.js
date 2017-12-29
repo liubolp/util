c4cf3c7c1be30d7eb1a1feeae153fbc93bb51ea5
liubo <liubolp@icloud.com>
2017-12-29 18:37:48 +0800

    修改多名片

--- lm/src/multiple-business-card/js/index.js
+++ lm/src/multiple-business-card/js/index.js
@@ -1,96 +1,92 @@
 window.addEventListener('load', function () {
   $(function () {
-    // 为每个名片更新背景图片
-    $('.box').each(function (e) {
-      var url = $(this).attr('data-img')
-      $(this).css({'background-image': 'url(' + url + ')'})
-    })
-    // 名片选择和添加
-    var selected
-    $('.card-list').on('click', 'li', function (e) {
-      if ($(this).hasClass('add')) {
-        $('a.modify').attr('href', 'javascript:;')
-          .removeClass('active')
-        selected = null
-        // todo 跳转到添加页面
-        $('.modal-add').show()
-      } else {
-        $(this).addClass('selected')
-          .siblings().removeClass('selected')
-        // todo 获取当前选中名片的关键信息，用于跳转到修改页面的参数
-        selected = $(this).attr('data-something')
-        // todo 将获取的参数放置在a.modify中用于跳转
-        var href = '#'
-        $('a.modify').attr('href', href)
-          .addClass('active')
+    var card = {
+      data: {},
+      init () {
+        this.attachEvent()
+        this.methods.getBackground()
+      },
+      attachEvent () {
+        // 保存新增名片信息
+        $('.add-save').click(function (e) {
+          var name = $(this).prev().val()
+          if (name) {
+            card.methods.addCard(name)
+          }
+          $('.modal-add').hide()
+        })
+        // 新增输入信息处理
+        $('.modal-add input').on('input', function (e) {
+          var val = $(this).val() ? '保存' : '取消'
+          $(this).next().html(val)
+        })
+        // 搜索处理
+        $('.search-bar .search').click(function (e) {
+          var value = $(this).prev().val()
+          card.methods.searchCard(value)
+        })
+        // 删除和修改
+        $('.card-list').on('click', '.delete', function (e) { // 删除
+          card.methods.deleteCard($(this).parents('li'))
+        })
+          .on('click', '.edit', function (e) { // 修改
+            card.methods.editCard($(this).parents('li'))
+          })
+          .on('click', '.use', function (e) { // 设为默认
+            card.methods.setDefault($(this).parents('li'))
+          })
+          .on('click', 'div.add', function (e) { // 新增名片
+            $('.modal-add').show()
+          })
+      },
+      methods: {
+        // 为每个名片更新背景图片
+        getBackground () {
+          $('.card-list .content').each(function (e) {
+            var url = $(this).attr('data-src')
+            $(this).css({'background-image': 'url(' + url + ')'})
+          })
+        },
+        /**
+         * 设为默认
+         * @param target {Object} 当前操作的li标签Jquery对象
+         */
+        setDefault (target) {
+          $('.card-list .use').removeClass('current')
+          target.find('.use').addClass('current')
+          // todo
+        },
+        /**
+         * 添加名片
+         * @param name {String} 新增名片名称
+         */
+        addCard (name) {
+          console.log(name)
+          // todo 添加名片操作
+        },
+        // 搜索名片
+        searchCard (name) {
+          console.log(name)
+          // TODO
+        },
+        /**
+         * 删除名片
+         * @param target {Object} 当前操作的li标签Jquery对象
+         */
+        deleteCard (target) {
+          console.log(target)
+          // todo
+        },
+        /**
+         * 修改名片
+         * @param target {Object} 当前操作的li标签Jquery对象
+         */
+        editCard (target) {
+          console.log(target)
+          // todo
+        }
       }
-    }).on('click', '.view button', function (e) { // 切换正在使用的名片
-      $('.view .current').removeClass('current')
-      $(this).addClass('current')
-      var target = $(this).parent().parent() // li>.box
-      // todo 将当前选中的信息更新到后台
-    })
-    // 设为默认背景
-    $('button.default').click(function (e) {
-      e.preventDefault()
-      $(this).parent().css({'background-image': 'none'})
-      //  todo 发请求到后台更新数据
-    })
-    // 修改名片
-    $('a.modify').click(function (e) {
-      if (!selected) { // 如果没有选择任何名片
-        e.preventDefault()
-        weui.alert('请选择一个名片后再试')
-      }
-    })
-    // 删除名片
-    $('a.delete').click(function (e) {
-      var target = $('.card-list li.selected')
-      // todo 获取目标元素中的必要信息然后调用删除
-      if (!target) { // 如果要获取的信息不存存在
-        weui.alert('请选择一个名片')
-        return
-      }
-      weui.confirm(`你确定都要删除这个名片吗？`, confirm, cancel, {
-        title: '删除名片'
-      })
-      // 确认删除
-      function confirm () {
-        // todo something
-      }
-      // 取消删除
-      function cancel () {
-        // todo something
-      }
-    })
-    // 搜索位置处理
-    $('.input-box input').blur(function (e) {
-      if ($(this).val()) {
-        $(this).parent().addClass('fixed')
-      } else {
-        $(this).parent().removeClass('fixed')
-      }
-    })
-    // 手动搜索处理
-    $('.input-box .search').click(function (e) {
-      var val = $(this).prev().val()
-      if (val) {
-        // todo 进行搜索操作
-        console.log(val)
-      }
-    })
-    // 保存新增名片信息
-    $('.add-save').click(function (e) {
-      var name = $(this).val()
-      if (name) {
-        // todo 保存到后台
-      }
-      $('.modal-add').hide()
-    })
-    // 输入信息
-    $('.modal-add input').on('input', function (e) {
-      var val = $(this).val() ? '保存' : '取消'
-      $(this).next().html(val)
-    })
+    }
+    card.init()
   })
 })
