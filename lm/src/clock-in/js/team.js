a485fe97929faf3732d040a00d09c9a005f6ace7
liubo <liubolp@icloud.com>
2018-01-29 18:30:25 +0800

    团队打卡页面

---
+++ lm/src/clock-in/js/team.js
@@ -0,0 +1,124 @@
+window.addEventListener('DOMContentLoaded', function () {
+  var app = {
+    data: {
+      canCreateTeam: false, // 是否可以创建团队
+      hasTeam: true, // 是否已经有团队
+      joinOther: false, // 加入其它团队
+      target: null // 排名用户点击时的身份信息
+    },
+    init () {
+      this.attachEvent()
+    },
+    attachEvent () {
+      // 打开菜单
+      $('.name-box .toggle').click(function (e) {
+        $('.menu-box').show()
+      })
+      // 菜单点击操作
+      $('.menu-box').on('click', 'li', function (e) {
+        e.stopPropagation()
+        if ($(this).hasClass('more')) { // 更多
+          console.log('更多')
+        } else if ($(this).hasClass('create')) { // 创建
+          app.methods.createTeam()
+        } else if ($(this).hasClass('exit')) { // 退出
+          $('.modal-exit').show()
+        }
+        $(e.delegateTarget).hide()
+      }).click(function (e) {
+        $(this).hide()
+      })
+      // 提醒好友打卡
+      $('.list').on('click', '.notice', function (e) {
+        app.data.target = $(this).attr('data-id')
+        app.methods.inviteFriends(2)
+      })
+      // 加入团队
+      $('button.join').click(function () {
+        if (!app.data.hasTeam) {
+          $('.modal-join').show()
+        } else if (app.data.joinOther) {
+          $('.modal-join-other').show()
+        } else { // 邀请好友
+          app.methods.inviteFriends(0)
+        }
+      })
+      // 创建团队弹窗操作
+      $('.modal-create').on('click', '.confirm,.cancel,.mask', function (e) {
+        if ($(this).hasClass('confirm')) { // 邀请好友打卡
+          $('.modal-invite').show()
+          app.methods.inviteFriends(1)
+        }
+        $(e.delegateTarget).hide()
+      })
+      // 邀请好友操作
+      $('.modal-invite').on('click', '.confirm,.cancel,.mask', function (e) {
+        $(e.delegateTarget).hide()
+      })
+      // 退出团队操作
+      $('.modal-exit').on('click', '.confirm,.cancel,.mask', function (e) {
+        if ($(this).hasClass('confirm')) { // 退出团队
+          app.methods.exitTeam()
+        }
+        $(e.delegateTarget).hide()
+      })
+      // 加入团队操作
+      $('.modal-join-other,.modal-join').on('click', '.confirm,.cancel,.mask', function (e) {
+        if ($(this).hasClass('confirm')) { // 加入
+          app.methods.joinTeam()
+        }
+        $(e.delegateTarget).hide()
+      })
+      // 关闭引导
+      $('.modal-share .close').click(function (e) {
+        $('.modal-share').hide()
+      })
+    },
+    methods: {
+      /**
+       * 加入团队
+       */
+      joinTeam () {
+        console.log('加入团队')
+      },
+      /**
+       * 创建团队
+       */
+      createTeam () {
+        if (app.data.canCreateTeam) {
+          // todo
+        } else {
+          $('.modal-create').show()
+        }
+      },
+      /**
+       * 邀请好友
+       * @param type { Number } 0:邀请好友加入团队；1邀请海报；2提醒好友打卡
+       */
+      inviteFriends (type) {
+        switch (type) {
+          case 0:
+            console.log('喊好友加入团队')
+            $('.modal-share').show()
+            break
+          case 1:
+            console.log('邀请海报')
+            break
+          case 2:
+            var id = app.data.target
+            console.log('提醒打卡', id)
+            break
+          default:
+            break
+        }
+      },
+      /**
+       * 退出团队
+       */
+      exitTeam () {
+        console.log('退出')
+      }
+    }
+  }
+  app.init()
+})
--- lm/src/shangmai/js/index.js
+++ lm/src/shangmai/js/index.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function () {
+window.addEventListener('DOMContentLoaded', function () {
   var app = {
     data: {
     },
@@ -20,8 +20,58 @@ window.addEventListener('load', function () {
           modal.addClass('show')
         })
       })
+      // 过滤操作
+      $('.filters').on('click', '.container', function (e) {
+        var type
+        if ($(this).hasClass('industry')) { // 选行业
+          type = 'industry'
+        }
+        else if ($(this).hasClass('city')) { // 选城市
+          type = 'city'
+        } else { // 认证方式
+          type = 'verify'
+        }
+        app.methods.filterBy(type, $(this).find('.name'))
+      })
+      // 加微信和好友
+      $('.sm-list').on('click', 'button', function (e) {
+        if ($(this).hasClass('wechat')) { // 加微信
+          console.log('加微信')
+        }
+        if ($(this).hasClass('friend')) { // 加好友
+          console.log('加好友')
+        }
+      })
     },
     methods: {
+      /**
+       * 筛选过滤操作
+       * @param type { String } 'industry':行业;'city'：城市;'verify':认证
+       * @param target { Object } 需要更改的Jquery对象
+       */
+      filterBy (type, target) {
+        console.log(type)
+        weui.picker([{
+          label: '飞机票',
+          value: 0,
+          disabled: true // 不可用
+        }, {
+          label: '火车票',
+          value: 1
+        }, {
+          label: '汽车票',
+          value: 3
+        }, {
+          label: '公车票',
+          value: 4
+        }], {
+          container: 'body',
+          defaultValue: [3],
+          onConfirm: function onConfirm (result) {
+            target.text(result[0].label)
+          }
+        })
+      }
     }
   }
   app.init()
